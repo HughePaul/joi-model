0c473d1d88250211c0cf0b32ce3df1303232367d
Paul Winkler <paul.winkler@grapplemobile.com>
2014-02-13 15:03:58 +0000

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,54 @@ joi-model
 =========
 
 Model encapsulation of a Joi schema
+
+[![Build Status](https://secure.travis-ci.org/hughepaul/joi-model.png)](http://travis-ci.org/hughepaul/joi-model)
+
+# Usage
+
+First create a schema
+
+```javascript
+var Joi = require('joi');
+
+var schema = {
+    a: Joi.string().required(),
+    b: Joi.string().min(4)
+};
+```
+
+Then create a model class for that schema:
+
+```javascript
+var joiModel = require('joi-model');
+
+var MyModel = joiModel(schema);
+```
+
+Then then create a document based on the model:
+
+```javascript
+var document = new MyModel();
+
+document.setData({
+    a: 'hello',
+    b: 'world'
+});
+
+document.a = 'test';
+document.b = 'another';
+
+document.a = 3; // throws a type error
+
+document.a = null; // throws a required error
+
+document.b = 'hel'; // throws a length error
+
+JSON.stringify(document); // '{"a":"test","b":"another"}'
+
+var document = new MyModel({
+    a: 'hello',
+    b: 'world'
+});
+
+```
---
+++ test/index.js
@@ -0,0 +1,233 @@
+// Load Modules
+var Lab = require('lab');
+var Joi = require('joi');
+var joiModel = require('../lib');
+
+// Test shortcuts
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+describe('joiModel', function() {
+
+    it('creates a model class from a schema', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c', '6'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        expect(SchemaModel).to.not.equal(null);
+
+        done();
+    });
+
+    it('creates a model from a model class', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj);
+
+        expect(m.a).to.equal(obj.a);
+        expect(m.b).to.equal(obj.b);
+        expect(m.c).to.equal(obj.c);
+
+        done();
+    });
+
+    it('sets value of a model', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj);
+
+        m.b = 'c';
+
+        expect(m.b).to.equal('c');
+
+        done();
+    });
+
+    it('converts values to the correct type', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema, {
+            modify: true
+        });
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj, {
+            convert: true
+        });
+
+        m.a = '3';
+
+        expect(m.a).to.equal(3);
+
+        done();
+    });
+
+    it('throws an error when the wrong type is set', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c', '6'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj);
+
+        var err;
+        try {
+            m.a = '5';
+            m.b = 8;
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+
+        done();
+    });
+
+    it('throws an error when a model is created with data of the wrong type', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c', '6'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj);
+
+        var err;
+        try {
+            m.a = '5';
+            m.b = 8;
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+
+        done();
+    });
+
+    it('throws an error if a recursive value is set to the wrong type', function(done) {
+
+        var schema = {
+            a: Joi.string().valid('a', 'b', 'c'),
+            b: Joi.object({
+                b1: Joi.string().required(),
+                b2: Joi.string().email().required()
+            }),
+            c: Joi.array().includes(Joi.string()).required().min(1)
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 'a',
+            b: {
+                b1: 'hello',
+                b2: 'world@test.com'
+            },
+            c: ['string']
+        };
+
+        var m = new SchemaModel(obj);
+
+        m.b.b2 = 'test@blah.com';
+
+        var err;
+        try {
+            m.b.b2 = 'world';
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+
+        done();
+    });
+
+
+    it('produces clean json from a polluted object', function(done) {
+
+        var schema = {
+            a: Joi.string(),
+            b: Joi.string(),
+            c: Joi.string().required()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 'aa',
+            c: 'cc'
+        };
+
+        var m = new SchemaModel(obj);
+
+        m.c = 'testing';
+        m.d = 'Hello World';
+
+        expect(JSON.stringify(m)).to.equal('{"a":"aa","c":"testing"}');
+
+        done();
+    });
+
+
+});
