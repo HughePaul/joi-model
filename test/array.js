fa1ef701d2c778bb471878dd1f19720baf0ee83e
Paul Winkler <paul.winkler@grapplemobile.com>
2014-02-13 18:56:10 +0000

    Add support for properly processing arrays of objects

---
+++ test/array.js
@@ -0,0 +1,240 @@
+// Load Modules
+var Lab = require('lab');
+var Joi = require('joi');
+var joiModel = require('../lib');
+var JoiArray = require('../lib/joiarray');
+
+// Test shortcuts
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+describe('JoiArray', function() {
+
+    it('loads an array based on an array schema', function(done) {
+
+        var schema = Joi.array().includes(Joi.string());
+
+        var arr = ['test1', 'test2'];
+
+        var jarray = new JoiArray(schema, null, arr);
+
+        expect(jarray.length).to.equal(2);
+        expect(jarray[0]).to.equal('test1');
+        expect(jarray[1]).to.equal('test2');
+
+        done();
+    });
+
+    it('allows adding items with push and unshift', function(done) {
+
+        var schema = Joi.array().includes(Joi.string());
+
+        var arr = ['test1'];
+
+        var jarray = new JoiArray(schema, null, arr);
+
+        jarray.push('post');
+        jarray.unshift('pre');
+
+        expect(jarray.length).to.equal(3);
+        expect(jarray[0]).to.equal('pre');
+        expect(jarray[2]).to.equal('post');
+
+        done();
+    });
+
+    it('allows removeing items with pop and shift', function(done) {
+
+        var schema = Joi.array().includes(Joi.string());
+
+        var arr = ['test1', 'test2', 'test3'];
+
+        var jarray = new JoiArray(schema, null, arr);
+
+        var popped = jarray.pop();
+        var shifted = jarray.shift();
+
+        expect(jarray.length).to.equal(1);
+        expect(shifted).to.equal('test1');
+        expect(jarray.get(0)).to.equal('test2');
+        expect(popped).to.equal('test3');
+
+        done();
+    });
+
+    it('loads an array based on an array schema', function(done) {
+
+        var schema = Joi.array().includes(Joi.string());
+
+        var arr = ['test1', 'test2'];
+
+        var jarray = new JoiArray(schema, null, arr);
+
+        expect(jarray.length).to.equal(2);
+        expect(jarray[0]).to.equal('test1');
+        expect(jarray[1]).to.equal('test2');
+
+        done();
+    });
+
+    it('loads an array based on an array schema including an object', function(done) {
+
+        var schema = Joi.array().includes(Joi.object({
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c'),
+            c: Joi.string().email().optional()
+        }));
+
+        var arr = [{
+            a: 1,
+            b: 'b',
+            c: 'test@email.com'
+        }, {
+            a: 2,
+            b: 'c',
+            c: 'blah@test.com'
+        }];
+
+        var jarray = new JoiArray(schema, null, arr);
+
+        expect(jarray.length).to.equal(2);
+        expect(jarray[0].b).to.equal('b');
+        expect(jarray.get(1).c).to.equal('blah@test.com');
+
+        done();
+    });
+
+    it('does not support mixing array types', function(done) {
+
+        var schema = Joi.array().includes(Joi.string(), Joi.object());
+
+        var arr = ['test1', 'test2'];
+
+        var err;
+        try {
+            var jarray = new JoiArray(schema, null, arr);
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+        expect(err.message).to.contain('does not support mixing object models with other types');
+
+        done();
+    });
+
+    it('does not support more than one object type', function(done) {
+
+        var schema = Joi.array().includes(Joi.object(), Joi.object());
+
+        var arr = ['test1', 'test2'];
+
+        var err;
+        try {
+            var jarray = new JoiArray(schema, null, arr);
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+        expect(err.message).to.contain('only supports one object model type per array');
+
+        done();
+    });
+
+    it('will throw an error if the loaded value doesnt match the schema', function(done) {
+
+        var schema = Joi.array().includes(Joi.string());
+
+        var arr = ['test1', 1];
+
+        var err;
+        try {
+            var jarray = new JoiArray(schema, null, arr);
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+        expect(err.message).to.contain('does not match any of the allowed types');
+
+        done();
+    });
+
+    it('will throw an error if the loaded object doesnt match the schema', function(done) {
+
+        var schema = Joi.array().includes(Joi.object({
+            a: Joi.string().required(),
+            b: Joi.number().required()
+        }));
+
+        var arr = [{
+            a: 'b',
+            b: 3
+        }, {
+            a: 'd'
+        }];
+
+        var err;
+        try {
+            var jarray = new JoiArray(schema, null, arr);
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+        expect(err.message).to.contain('does not match any of the allowed types');
+
+        done();
+    });
+
+    it('will throw an error if the set object doesnt match the schema', function(done) {
+
+        var schema = Joi.array().includes(Joi.object({
+            a: Joi.string().required(),
+            b: Joi.number().required()
+        }));
+
+        var jarray = new JoiArray(schema, null);
+
+        jarray.set(4, {
+            a: 'b',
+            b: 3
+        });
+
+        var err;
+        try {
+            jarray.set(4, {
+                a: 'b',
+                b: 'test'
+            });
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+        expect(err.message).to.contain('does not match any of the allowed types');
+
+        done();
+    });
+
+    it('will generate nice clean JSON output', function(done) {
+
+        var schema = Joi.array().includes(Joi.object({
+            a: Joi.string().required(),
+            b: Joi.number().required()
+        }));
+
+        var jarray = new JoiArray(schema, null);
+        jarray.set(4, {
+            a: 'b',
+            b: 3
+        });
+
+        var result = jarray.toString();
+
+        expect(result).to.equal('[null,null,null,null,{"a":"b","b":3}]');
+
+        done();
+    });
+
+
+});
---
+++ test/model.js
@@ -0,0 +1,383 @@
+// Load Modules
+var Lab = require('lab');
+var Joi = require('joi');
+var joiModel = require('../lib');
+
+// Test shortcuts
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+describe('joiModel', function() {
+
+    it('creates a model class from a schema', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c', '6'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        expect(SchemaModel).to.not.equal(null);
+
+        done();
+    });
+
+    it('creates a model from a model class', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj);
+
+        expect(m.a).to.equal(obj.a);
+        expect(m.b).to.equal(obj.b);
+        expect(m.c).to.equal(obj.c);
+
+        done();
+    });
+
+    it('sets value of a model', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj);
+
+        m.b = 'c';
+
+        expect(m.b).to.equal('c');
+
+        done();
+    });
+
+    it('sets updates only the specified data when asked to update', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj);
+
+        m.updateData({
+            b: 'c',
+            a: 3
+        });
+
+        expect(m.a).to.equal(3);
+        expect(m.b).to.equal('c');
+        expect(m.c).to.equal('joe@example.com');
+
+        done();
+    });
+
+    it('converts values to the correct type', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema, {
+            modify: true
+        });
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj, {
+            convert: true
+        });
+
+        m.a = '3';
+
+        expect(m.a).to.equal(3);
+
+        done();
+    });
+
+    it('throws an error when the wrong type is set', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c', '6'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj);
+
+        var err;
+        try {
+            m.a = '5';
+            m.b = 8;
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+
+        done();
+    });
+
+    it('throws an error when a model is created with data of the wrong type', function(done) {
+
+        var schema = {
+            a: Joi.number().min(0).max(3).without('none'),
+            b: Joi.string().valid('a', 'b', 'c', '6'),
+            c: Joi.string().email().optional()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 1,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        var m = new SchemaModel(obj);
+
+        var err;
+        try {
+            m.a = '5';
+            m.b = 8;
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+
+        done();
+    });
+
+    it('throws an error if a recursive value is set to the wrong type', function(done) {
+
+        var schema = {
+            a: Joi.string().valid('a', 'b', 'c'),
+            b: Joi.object({
+                b1: Joi.string().required(),
+                b2: Joi.string().email().required()
+            }),
+            c: Joi.array().includes(Joi.string()).required().min(1)
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 'a',
+            b: {
+                b1: 'hello',
+                b2: 'world@test.com'
+            },
+            c: ['string']
+        };
+
+        var m = new SchemaModel(obj);
+
+        m.b = {
+            b1: 'firstSubItem',
+            b2: 'valid@email.com'
+        };
+
+        m.b.b2 = 'test@blah.com';
+
+        var err;
+        try {
+            m.b.b2 = 'world';
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+
+        expect(m.b.b2).to.equal('test@blah.com');
+
+        done();
+    });
+
+
+    it('produces clean json from a polluted object', function(done) {
+
+        var schema = {
+            a: Joi.string(),
+            b: Joi.string(),
+            c: Joi.string().required()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var obj = {
+            a: 'aa',
+            c: 'cc'
+        };
+
+        var m = new SchemaModel(obj);
+
+        m.c = 'testing';
+        m.d = 'Hello World';
+
+        expect(JSON.stringify(m)).to.equal('{"a":"aa","c":"testing"}');
+
+        expect('' + m).to.equal('{"a":"aa","c":"testing"}');
+
+        done();
+    });
+
+    it('does not allow an invalid schema object when creating a model', function(done) {
+
+        var schema = "bad object";
+
+        var err;
+        try {
+            var SchemaModel = joiModel(schema);
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.exist;
+
+        done();
+    });
+
+
+    it('allows multiple types to be specified for a property', function(done) {
+
+        var schema = {
+            username: Joi.string().alphanum().min(3).max(30).with('birthyear').required(),
+            password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).without('access_token'),
+            access_token: [Joi.string(), Joi.number()],
+            birthyear: Joi.number().integer().min(1900).max(2013),
+            email: Joi.string().email()
+        };
+
+        var SchemaModel = joiModel(schema);
+
+        var m = new SchemaModel({
+            username: 'abc',
+            birthyear: 1994
+        });
+
+        expect(m.birthyear).to.equal(1994);
+
+        done();
+    });
+
+    it('will load in data to a model including arrays of objects', function(done) {
+
+        var schema = {
+            name: Joi.string(),
+            children: Joi.array().includes(Joi.object({
+                name: Joi.string().required(),
+                age: Joi.number().required()
+            }))
+        };
+
+        var MyModel = joiModel(schema);
+
+        var m = new MyModel({
+            name: 'Paul',
+            children: [{
+                name: 'Arnald',
+                age: 10
+            }, {
+                name: 'Fred',
+                age: 12
+            }]
+        });
+
+        m.children = [{
+            name: 'Mandy',
+            age: 11
+        }, {
+            name: 'Lucy',
+            age: 13
+        }];
+
+        done();
+    });
+
+    it('will throw an error if an array object doesnt match the model', function(done) {
+
+        var schema = {
+            name: Joi.string(),
+            children: Joi.array().includes(Joi.object({
+                name: Joi.string().required(),
+                age: Joi.number().required()
+            }))
+        };
+
+        var MyModel = joiModel(schema);
+
+        var m = new MyModel({
+            name: 'Brad'
+        });
+
+        m.children = [{
+            name: 'Mandy',
+            age: 11
+        }, {
+            name: 'Lucy',
+            age: 12
+        }];
+
+        m.children[0].age = 14;
+        var err;
+        try {
+            m.children[1].age = 'test';
+        } catch(e) {
+            err = e;
+        }
+
+        expect(err).to.exist;
+        expect(err.message).to.contain('must be a number');
+
+        done();
+    });
+
+
+
+});
